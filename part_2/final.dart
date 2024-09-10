bcc257e5cd18c51f5825635f45efb9c4c7440981
juan12331 <146020152+juan12331@users.noreply.github.com>
2024-09-09 21:09:09 -0300

    Update final.dart

--- part_2/final.dart
+++ part_2/final.dart
@@ -1,1 +1,26 @@
-dart
+// aqui é uma parte mais importante onde vê future async e await
+
+void main ()  async{
+  //Função precisa ser assincrona para usar o await
+
+  Future<String> cepFuture = getCepByName('Rua jk');
+  // essa é uma variavel que recebe o valor da função getCepByName
+
+  late String cep;
+  // Usamos late para variaveis que serão inicializadas depois
+
+    cep = await cepFuture;
+    //para printar o resultado verdadeiro da função retorne usamos uma função then (como se fosse um fetch)
+    //Ou podemos fazer isso
+
+    print(cep);
+     //isso vai retornar apenas Instance of 'Future<String>'
+
+}
+
+Future<String> getCepByName(String nome) {
+  //Simula uma requisição na internet
+  return Future.value('0003002001');
+}
+
+//pode parecer meio confuso mas sla eu aprendo dps tanto faz
